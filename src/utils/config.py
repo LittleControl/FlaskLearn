ffeb1804cde733ec14805ce0f1c7b9da501a3959
LittleControl <i@littlecontrol.io>
2021-01-15 11:56:44 +0800

    database_name&passwd

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,7 +2,7 @@
 
 # database 
 POSTGRES_DATABASE = 'test_db'
-POSTGRES_USERNAME = 'iyuxuan'
-POSTGRES_PASSWORD = 'iyuxuan'
+POSTGRES_USERNAME = 'avalon'
+POSTGRES_PASSWORD = 'avalon'
 POSTGRES_HOST = 'localhost'
 POSTGRES_PORT = 5432
